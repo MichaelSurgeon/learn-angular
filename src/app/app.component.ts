b4b34ed0698749fd82d13fd2ec469d09111a042d
Michael Surgeon <michaeljamessurgeon@gmail.com>
2022-02-20 22:14:53 +0000

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learn-angular';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  addMember() {
+
+    if(!this.newMemberName) {
+      this.errorMessage = "Name can't be empty"
+      return;
+    } else {
+      this.errorMessage = '';
+    }
+
+    this.members.push(this.newMemberName)
+    this.newMemberName = '';
+    console.log(this.members)
+  }
+
+  onInput(member: string)
+  {
+    this.newMemberName = member;
+    console.log(this.newMemberName)
+  }
+
+  clearMembers() {
+    this.members.length = 0;
+  }
+  
 }
